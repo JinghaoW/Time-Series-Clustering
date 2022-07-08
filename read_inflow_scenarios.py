372780ef7057babeeccf3113df236a0627570921
jinghaow <wjh624129304"@gmail.com>
2022-07-08 12:27:46 +0200

    new update of new setting

--- read_inflow_scenarios.py
+++ read_inflow_scenarios.py
@@ -16,14 +16,14 @@ def read_inflow_scenarios():
     for year in range(51):
         inflow['regulated'][year] = {}
         for i in range(49901, 49913):
-            data_1 = data[str(i)].iloc[year * 52:(year) * 52+52]## each time read 55 digit
+            data_1 = data[str(i)].iloc[year * 52:(year) * 52+52]## each time read 52 digit
             inflow['regulated'][year][str(i)] = data_1.reset_index(drop=True)
     ## unregulated
     inflow['unregulated'] = {}
     for year in range(51):
         inflow['unregulated'][year] = {}
         for i in range(49901, 49913):
-            data_2 = udata[str(i)].iloc[year * 52:(year) * 52+52]## each time read 54 digit
+            data_2 = udata[str(i)].iloc[year * 52:(year) * 52+52]## each time read 52 digit
             inflow['unregulated'][year][str(i)] = data_2.reset_index(drop=True)
 
     return inflow
